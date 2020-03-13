79c35d987d07810d4889a110852e856605aad086
pedromtelho <pedromtelho@hotmail.com>
2020-03-13 15:35:53 -0300

    sending data is not working yet

--- ehlo55front/lib/views/MarketView/ConfirmationScreen.dart
+++ ehlo55front/lib/views/MarketView/ConfirmationScreen.dart
@@ -16,40 +16,177 @@ class ConfirmationScreen extends StatefulWidget {
 }
 
 class _ConfirmationScreenState extends State<ConfirmationScreen> {
-  var sendJson = {};
-  String newBrand = "";
-  String newType = "";
-  String newQuant = "";
+  var sendJson;
+  TextEditingController brandInputController = new TextEditingController();
+  var brandName;
+  TextEditingController typeInputController = new TextEditingController();
+  var typeName;
+  TextEditingController quantityInputController = new TextEditingController();
+  var quantityValue;
 
-  @override
-  Widget build(BuildContext context) {
-    InfoPayment args = ModalRoute.of(context).settings.arguments;
-    var jsonArg = json.decode(args.data);
-
+  composeJson() {
     sendJson = {
       "signed_data": {
         "updateDate": "2020-03-10T22:44:41.651Z",
         "lastUpdateHash": "pegar automaticamente (info shippment)",
         "shippmentUpdateDescriptor": {
           "transactionType": "1",
-          "productBrand": newBrand != null
-              ? newBrand
-              : jsonArg["signed_data"]["shippmentUpdateDescriptor"]
-                      ["productBrand"]
-                  .toString(),
-          "productType": newType != null
-              ? newType
-              : jsonArg["signed_data"]["shippmentUpdateDescriptor"]
-                      ["productType"]
-                  .toString(),
-          "quantity": newQuant != null
-              ? newQuant
-              : jsonArg["signed_data"]["shippmentUpdateDescriptor"]["quantity"]
-                  .toString()
+          "productBrand": brandName,
+          "productType": typeName,
+          "quantity": quantityValue
         },
         "signature": "Fazer do algoritimo"
       }
     };
+
+    return sendJson;
+  }
+
+  Widget buildTextInputRow(
+      String label,
+      TextEditingController textEditingController,
+      String hintText,
+      TextInputType textInputType) {
+    return Padding(
+        padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 5.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    TextMont(
+                      fontWeight: FontWeight.w400,
+                      color: HexColor("#000000"),
+                      textAlign: TextAlign.justify,
+                      text: label,
+                      textSize: 20,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: TextField(
+                              onSubmitted: (String str) {
+                                setState(() {
+                                  switch (label) {
+                                    case 'Marca do produto:':
+                                      this.brandName =
+                                          textEditingController.text;
+                                      break;
+                                    case 'Tipo do produto:':
+                                      this.typeName =
+                                          textEditingController.text;
+                                      break;
+                                    case 'Quantidade:':
+                                      this.quantityValue =
+                                          textEditingController.text;
+                                      break;
+                                    default:
+                                      break;
+                                  }
+                                });
+                              },
+                              textAlign: TextAlign.center,
+                              keyboardType: textInputType,
+                              maxLines: 2,
+                              enabled: true,
+                              decoration: InputDecoration(
+                                  border: UnderlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.blue[100])),
+                                  hintText: hintText),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ]));
+    // TextMont(
+    //   fontWeight: FontWeight.w400,
+    //   color: HexColor("#000000"),
+    //   textAlign: TextAlign.justify,
+    //   text: "Tipo do produto: ",
+    //   textSize: 20,
+    // ),
+    // Padding(
+    //   padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+    //   child: Row(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: <Widget>[
+    //       Expanded(
+    //         child: TextField(
+    //           onSubmitted: (e) {
+    //             setState(() {
+    //               newType = e;
+    //             });
+    //           },
+    //           textAlign: TextAlign.center,
+    //           keyboardType: TextInputType.multiline,
+    //           maxLines: 2,
+    //           enabled: true,
+    //           decoration: InputDecoration(
+    //               border: UnderlineInputBorder(
+    //                   borderSide:
+    //                       BorderSide(color: Colors.blue[100])),
+    //               hintText: jsonArg["signed_data"]
+    //                           ["shippmentUpdateDescriptor"]
+    //                       ["productType"]
+    //                   .toString()),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // ),
+    // TextMont(
+    //   fontWeight: FontWeight.w400,
+    //   color: HexColor("#000000"),
+    //   textAlign: TextAlign.justify,
+    //   text: "Quantidade: ",
+    //   textSize: 20,
+    // ),
+    // Padding(
+    //   padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+    //   child: Row(
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children: <Widget>[
+    //       Expanded(
+    //         child: TextField(
+    //           onSubmitted: (e) {
+    //             setState(() {
+    //               newQuant = e;
+    //             });
+    //           },
+    //           textAlign: TextAlign.center,
+    //           keyboardType: TextInputType.multiline,
+    //           maxLines: 2,
+    //           enabled: true,
+    //           decoration: InputDecoration(
+    //               border: UnderlineInputBorder(
+    //                   borderSide:
+    //                       BorderSide(color: Colors.blue[100])),
+    //               hintText: jsonArg["signed_data"]
+    //                           ["shippmentUpdateDescriptor"]
+    //                       ["quantity"]
+    //                   .toString()),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    InfoPayment args = ModalRoute.of(context).settings.arguments;
+    var jsonArg = json.decode(args.data);
 
     return Scaffold(
       backgroundColor: HexColor('#FFFFFF'),
@@ -70,134 +207,56 @@ class _ConfirmationScreenState extends State<ConfirmationScreen> {
                   textSize: 30,
                 ),
               ),
-              TextMont(
-                fontWeight: FontWeight.w200,
-                color: HexColor("#000000"),
-                textAlign: TextAlign.center,
-                text:
-                    "Caso possua alguma falha na entrega reescreva nos campos disponíveis abaixo",
-                textSize: 14,
-              ),
               Padding(
                 padding: EdgeInsets.all(10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    TextMont(
-                      fontWeight: FontWeight.w400,
-                      color: HexColor("#000000"),
-                      textAlign: TextAlign.justify,
-                      text: "Marca do produto: ",
-                      textSize: 20,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            child: TextField(
-                              onSubmitted: (e) {
-                                setState(() {
-                                  newBrand = e;
-                                });
-                              },
-                              textAlign: TextAlign.center,
-                              keyboardType: TextInputType.multiline,
-                              maxLines: 2,
-                              enabled: true,
-                              decoration: InputDecoration(
-                                  border: UnderlineInputBorder(
-                                      borderSide:
-                                          BorderSide(color: Colors.blue[100])),
-                                  hintText: jsonArg["signed_data"]
-                                              ["shippmentUpdateDescriptor"]
-                                          ["productBrand"]
-                                      .toString()),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    TextMont(
-                      fontWeight: FontWeight.w400,
-                      color: HexColor("#000000"),
-                      textAlign: TextAlign.justify,
-                      text: "Tipo do produto: ",
-                      textSize: 20,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            child: TextField(
-                              onSubmitted: (e) {
-                                setState(() {
-                                  newType = e;
-                                });
-                              },
-                              textAlign: TextAlign.center,
-                              keyboardType: TextInputType.multiline,
-                              maxLines: 2,
-                              enabled: true,
-                              decoration: InputDecoration(
-                                  border: UnderlineInputBorder(
-                                      borderSide:
-                                          BorderSide(color: Colors.blue[100])),
-                                  hintText: jsonArg["signed_data"]
-                                              ["shippmentUpdateDescriptor"]
-                                          ["productType"]
-                                      .toString()),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    TextMont(
-                      fontWeight: FontWeight.w400,
-                      color: HexColor("#000000"),
-                      textAlign: TextAlign.justify,
-                      text: "Quantidade: ",
-                      textSize: 20,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            child: TextField(
-                              onSubmitted: (e) {
-                                setState(() {
-                                  newQuant = e;
-                                });
-                              },
-                              textAlign: TextAlign.center,
-                              keyboardType: TextInputType.multiline,
-                              maxLines: 2,
-                              enabled: true,
-                              decoration: InputDecoration(
-                                  border: UnderlineInputBorder(
-                                      borderSide:
-                                          BorderSide(color: Colors.blue[100])),
-                                  hintText: jsonArg["signed_data"]
-                                              ["shippmentUpdateDescriptor"]
-                                          ["quantity"]
-                                      .toString()),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                child: TextMont(
+                  fontWeight: FontWeight.w200,
+                  color: HexColor("#000000"),
+                  textAlign: TextAlign.center,
+                  text:
+                      "Caso possua alguma falha na entrega reescreva nos campos disponíveis abaixo",
+                  textSize: 14,
                 ),
               ),
-              DragButton(
-                route: "/Valid",
-                object: InfoMarketPayment(sendJson),
-              )
+              Expanded(
+                child: SizedBox(
+                  height: 200.0,
+                  child: ListView(
+                    children: <Widget>[
+                      buildTextInputRow(
+                          'Marca do produto:',
+                          brandInputController,
+                          jsonArg["signed_data"]["shippmentUpdateDescriptor"]
+                                  ["productBrand"]
+                              .toString(),
+                          TextInputType.multiline),
+                      buildTextInputRow(
+                          'Tipo do produto:',
+                          typeInputController,
+                          jsonArg["signed_data"]["shippmentUpdateDescriptor"]
+                                  ["productType"]
+                              .toString(),
+                          TextInputType.multiline),
+                      buildTextInputRow(
+                          'Quantidade:',
+                          quantityInputController,
+                          jsonArg["signed_data"]["shippmentUpdateDescriptor"]
+                                  ["quantity"]
+                              .toString(),
+                          TextInputType.number),
+                    ],
+                  ),
+                ),
+              ),
+              FlatButton(
+                  onPressed: () {
+                    print(composeJson());
+                  },
+                  child: new Text('test'))
+              // DragButton(
+              //   route: "/Valid",
+              //   object: InfoMarketPayment(composeJson()),
+              // )
             ],
           ),
         ),
